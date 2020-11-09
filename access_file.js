78c42e865585f614fddb68888086211cdd572780
sypherce <sypherce@gmail.com>
2020-11-09 12:37:35 -0500

    Formatting changes

--- access_file.js
+++ access_file.js
@@ -1,20 +1,24 @@
 'use strict';
 export default function AccessFile(_filename) {
 	const php_file = "../access_file.php?";
+	const access_freq = 1000;
 	var file_object = {
 		filename: _filename,
 		contents: "",
 		last_access: null,
 		read : function(_filename) {
 			var this_access = (new Date);
+
 			if(!file_object.last_access)
-				file_object.last_access = this_access - 1000;
-			if(this_access - file_object.last_access >= 1000) {
+				file_object.last_access = this_access - access_freq;
+			if(this_access - file_object.last_access >= access_freq) {
 				file_object.last_access = this_access;
+
 				if(typeof _filename !== "undefined" && _filename !="")
 					this.filename = _filename;
 				if(!this.filename.startsWith(php_file))
 					this.filename = php_file + this.filename;
+
 				var request = new XMLHttpRequest();
 				request.onreadystatechange = function() {
 					if (this.readyState == 4) {
--- overlay/index.php
+++ overlay/index.php
@@ -6,6 +6,6 @@
 <link rel="stylesheet" href="main.css">
 <script type="module" src="main_<?php echo $_GET["game"];?>.js?time=<?php echo time();?>"></script>
 </head>
-<body onload="init('assets/<?php echo $_GET["game"];?>/background.png', 1920, 1080, 20);">
+<body onload="init('assets/<?php echo $_GET["game"];?>/background.png', 1920, 1080, 60);">
 </body>
 </html>
